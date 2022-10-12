ad37f47a9c9ce926a423c7a49379087347cb526f
Salma Belal <100041807+SalmaBelal@users.noreply.github.com>
2022-10-11 22:04:30 -0400

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+group_md(){
+
+cd $1
+echo $1
+
+mdfiles=(*.md)
+if [ ${#mdfiles[@]} -eq 0 ]; then
+    echo "here"
+    return
+fi
+
+
+directories=(*/)
+if [ ${directories[@]} = "*/" ]; then
+    directories=()
+fi
+
+echo '' > toc
+for i in *.md
+do 
+echo "$i" >> toc
+done
+echo '' > ${PWD##*/}.md
+for i in $(cat toc)
+do
+cat $i >> ${PWD##*/}.md
+done
+
+for i in ${directories[@]}
+do 
+directory="${1}/${i}" 
+echo ${directory}
+group_md ${directory}
+done
+
+}
+
+group_md $(pwd)
+
